4980f155f9a0cf8ffd5d90e641769c547b703ec5
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-11 18:42:04 +0530

    implemented generics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use num_traits::Float;
+mod basket;
+use basket::Basket;
+
+fn solve<T: Float>(a: T, b: T) -> T {
+    (a.powi(2) + b.powi(2)).sqrt()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = 3.0;
+    let b = 4.0;
+    println!("{}",solve(a,b));
+
+    let basket = Basket::new(String::from("Basket"));
+    let num_basket = Basket::new(10);
+
+    println!("{:#?} {:#?}",basket,num_basket);
+
 }
